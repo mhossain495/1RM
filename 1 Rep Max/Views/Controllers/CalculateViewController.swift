667a58436ef452e43c7d07ec0f7ddd5eeabe9210
Mohammed Hossain <mohammed.hossain4@baruchmail.cuny.edu>
2020-12-01 17:46:47 -0500

    Add additional rep labels to display max weight for various rep ranges

--- 1 Rep Max/Views/Controllers/CalculateViewController.swift
+++ 1 Rep Max/Views/Controllers/CalculateViewController.swift
@@ -23,7 +23,18 @@ class CalculateViewController: UIViewController {
     @IBOutlet weak var repsTextField: UITextField!
     @IBOutlet weak var oneRepMax: UILabel!
     
-
+    // Left side labels to populate with weight value
+    @IBOutlet weak var twoRMLabel: UILabel!
+    @IBOutlet weak var threeRMLabel: UILabel!
+    @IBOutlet weak var fourRMLabel: UILabel!
+    @IBOutlet weak var fiveRMLabel: UILabel!
+    
+    // Right side labels to populate with weight value
+    @IBOutlet weak var sixRMLabel: UILabel!
+    @IBOutlet weak var eightRMLabel: UILabel!
+    @IBOutlet weak var tenRMLabel: UILabel!
+    @IBOutlet weak var twelveRMLabel: UILabel!
+    
 }
 
 //MARK: - UITextFieldDelegate
@@ -31,10 +42,12 @@ class CalculateViewController: UIViewController {
 extension CalculateViewController: UITextFieldDelegate {
 
     func textFieldDidEndEditing(_ textField: UITextField) {
-        // If weight and reps textfield are not nil convert the values to Float else use default value of 0
+       
+        // If weight and reps textfield are not nil, convert the values to Float or else use default value of 0
         let weightValue = Float(weightTextField.text!) ?? Float(0)
         let repsValue = Float(repsTextField.text!) ?? Float(0)
       
+        // Calculate one rep max with values obtained from weight and reps text fields
         let max = maxCalculator.calculateMax(weight: weightValue, reps: repsValue)
         oneRepMax.text = String(max)
 
