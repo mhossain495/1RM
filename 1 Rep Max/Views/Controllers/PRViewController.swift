296fc5d465ff71b09bec55273bccaa5a692ca878
Mohammed Hossain <mohammed.hossain4@baruchmail.cuny.edu>
2020-12-16 18:50:11 -0500

    Add UIImage and labels to prototype cell

---
+++ 1 Rep Max/Views/Controllers/PRViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PRViewController.swift
+//  1 Rep Max
+//
+//  Created by Mohammed Hossain on 12/16/20.
+//
+
+import UIKit
+
+class PRViewController: UIViewController {
+    
+    
+}
---
+++ 1 Rep Max/Views/HistoryTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  HistoryCell.swift
+//  1 Rep Max
+//
+//  Created by Mohammed Hossain on 12/15/20.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    
+
+}
