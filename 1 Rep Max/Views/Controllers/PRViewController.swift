96a4c8812c311d226c0253f95c7c9636d5df7965
Mohammed Hossain <mohammed.hossain4@baruchmail.cuny.edu>
2020-12-16 21:53:30 -0500

    Add TableView Delegate and DataSource

--- 1 Rep Max/Views/Controllers/PRViewController.swift
+++ 1 Rep Max/Views/Controllers/PRViewController.swift
@@ -7,7 +7,45 @@
 
 import UIKit
 
+
 class PRViewController: UIViewController {
+    
+    @IBOutlet weak var historyTableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        historyTableView.delegate = self
+        historyTableView.dataSource = self
+        
+    }
+    
+    
+    
+    
+}
+//MARK: - TableView Delegate
+
+extension PRViewController: UITableViewDelegate {
+    
+}
+
+
+
+
+
+//MARK: - TableView DataSource
+
+extension PRViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "historyCell", for: indexPath) as! HistoryTableViewCell
+        return cell
+    }
     
     
 }
